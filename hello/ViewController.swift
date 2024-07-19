c6beb2be6400d1998fc050304564841a5e5c260b
homeless <hmlssgd@gmail.com>
2024-07-19 14:11:14 +0300

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by MacBookAir on 19.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
